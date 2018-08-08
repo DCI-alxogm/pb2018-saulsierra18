26e8e32e6d92f2ae58033db8cbe26ac815c73ae5
saulsierra18 <se.sierraluna@ugto.mx>
2018-08-08 09:30:55 -0500

    Programa actualizado :D

---
+++ semana1/programa_hola.c
@@ -0,0 +1,13 @@
+/*
+ Esto es un comentario. me sirve para escribir informacion relevante del programa que estoy haciendo. Por ejemplo :
+Este programa imprime la típicia frase "Hola mundo en la pantalla"
+creado el 06/08/2018 por Saúl Sierra Luna 
+*/
+#include <stdio.h>
+int main ()
+{
+	printf("Hola soy Saúl y soy rescursador\n ");
+	printf("Este es el  primer programa de este magnífico curso\n ");
+
+}
+/*Termina este programa */
