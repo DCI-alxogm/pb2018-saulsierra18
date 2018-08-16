57cf62fa494d482d16a84f2ebedbaf3df6cb2ae7
saulsierra18 <se.sierraluna@ugto.mx>
2018-08-16 17:02:55 -0500

    Programa actualizado

--- semana2/primer_programa.c
+++ semana2/primer_programa.c
@@ -19,7 +19,7 @@ int main()
     printf("Introduce tu edad:\n");
     scanf("%i",&edad);
     edad2=edad+10;
-    printf("\n \t\t Tu edad es :%i\n",edad2);
+    printf("\n \t\t Tu edad en 2028 será :%i\n",edad2);
     printf("\t\t¡Felicidades ya estas viejo!\n");
     return 0;
 }
