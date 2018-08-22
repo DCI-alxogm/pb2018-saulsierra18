ed61058500c837d06a083d096daf304293906275
saulsierra18 <se.sierraluna@ugto.mx>
2018-08-22 09:19:24 -0500

    Actualizar#

---
+++ semana3/switch.c
@@ -0,0 +1,10 @@
+//
+//  switch.c
+//  
+//
+//  Created by Saul Sierra Luna on 22/08/18.
+//
+
+#include "switch.h"
+#indlude <stdio.h>
+
---
+++ semana3/switch.h
@@ -0,0 +1,13 @@
+//
+//  switch.h
+//  
+//
+//  Created by Saul Sierra Luna on 22/08/18.
+//
+
+#ifndef switch_h
+#define switch_h
+
+#include <stdio.h>
+
+#endif /* switch_h */
